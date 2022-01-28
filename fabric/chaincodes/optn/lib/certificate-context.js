ccd69e0ab05fb8c41286ce49e69a5216110aa702
ksanjaykumar1 <ksanjayk@protonmail.com>
2022-01-28 19:02:40 +0530

    subclass of Context to get the clientName from the transaction

---
+++ fabric/chaincodes/optn/lib/certificate-context.js
@@ -0,0 +1,22 @@
+const {Context} =require('fabric-contract-api')
+
+class CertificateContext extends Context{
+
+    constructor(){
+        super();
+    }
+
+    getClientName(){
+
+        const id = ctx.clientIdentity.getID()
+        console.log(`id: ${id}`)
+        console.log(typeof(id))
+        const data = id.split('/')
+        const client = data[2].split('=')
+        const clientName = client[1].split('::')
+        return clientName[0]
+
+    }
+}
+
+modules.exports = CertificateContext
