e2d60f28471273a714963424a736e7774595d5ce
ksanjaykumar1 <ksanjayk@protonmail.com>
2022-01-30 07:16:27 +0530

    added function to add organs and query organs with different parameters

--- fabric/chaincodes/optn/lib/optn.js
+++ fabric/chaincodes/optn/lib/optn.js
@@ -1,9 +1,10 @@
 const {Contract} = require('fabric-contract-api')
+const CertificateContext = require('./certificate-context')
 
 const oganObjType= 'Organ';
 const tissueObjType='Tissue';
 const PatientObjType= 'Patient'
-const ReqStatus = {Open:1, Closed:2, Withdraw:3}
+const organStatus = {Open:1, Accepted:2,Disposed:3, Transplanted:4}
 const organs=['Kidney','Liver','Heart', 'Lungs', 'Pancreas','Intestine']
 const tissues=['Cornea','Bones','Skin','Veins','Muscles','Tendons','Ligaments','Cartilage','Heart Valves']
 
@@ -20,39 +21,100 @@ const tissuesObject = toObject(tissues)
 
 class Optn extends Contract{
 
-    async initLedger(ctx)
+    createContext(){
+
+        return new CertificateContext();
+
+    }
+    async initLedger()
     {
         console.log('chaincode initialized')
         console.log('The accepted organ are ' +organs)
         console.log('The accepted tissue are ' +tissues)
     }
 
-    
+    async addOrgan(ctx, id, organName, donorID, time){
 
-    // async addOrgan(ctx, organName,){
+        if(!organsObject[organName])
+        {
+            throw new Error(`The following organs are accepted: ${organs}. Enter the organ name is this form`)
+        }
+        const opo = ctx.getClientName()
+        console.log(opo)
 
-    //     let organ ={
-    //         id: organID,
-    //         hospital: hospital,
-    //         donorID: donorID,
-    //         time:time,
-    //         status: ReqStatus.Open
-    //     }
+        const organID = ctx.stub.createCompositeKey(opo,[id])
+        const compositeKey = ctx.stub.createCompositeKey(organName,[opo,id])
+        
+        let organ ={
+            organID: organID,
+            opo: opo,
+            donorID: donorID,
+            organName:organName,
+            time:time,
+            status: organStatus.Open
+        }
+        const result = await this._keyExists(ctx,compositeKey)
+        if(result ){
+            throw new Error(`the organ ${organID} already exists`)
+        }
+        await ctx.stub.putState(compositeKey, JSON.stringify(organ))
+        // await ctx.stub.putState(organID,JSON.stringify(organ))
+        console.log(`Organ with id ${compositeKey} added`)
+        return organ
+    }
 
-    // }
+   
 
-    // async addOrganLabReports(ctx){
+    async queryOrgan(ctx, organName,id, opo){
 
-    // }
+        const compositeKey = ctx.stub.createCompositeKey(organName,[opo,id])
+        const organ= await ctx.stub.getState(compositeKey)
+        if(organ && organ.length >0)
+        {
+            return JSON.parse(organ.toString());
+        }
+        throw new Error(`Organ with ${compositeKey} doesn't exist`)
 
-    // async requestOrgan(ctx){
+    }
 
-    // }
+    async queryOrgansWithOrganName(ctx, organName){
 
-    // async decidePatient(ctx){
+        if(!organsObject[organName])
+        {
+            throw new Error(`The following organs are accepted: ${organs}. Enter the organ name is this form`)
+        }
+        const iteratorPromise = ctx.stub.getStateByPartialCompositeKey(organName,[])
+        let results =[]
+        for await ( const res of iteratorPromise){
+            const organ  = JSON.parse(res.value.toString())
+            results.push(organ)
+        }
+        return JSON.stringify(results)
+    }
 
-    // }
+    async queryOrgansWithOrganNameAndStatus(ctx, organName,status){
 
+        if(!organsObject[organName])
+        {
+            throw new Error(`The following organs are accepted: ${organs}. Enter the organ name is this form`)
+        }
+        const iteratorPromise = ctx.stub.getStateByPartialCompositeKey(organName,[])
+        let results =[]
+        for await ( const res of iteratorPromise){
+            const organ  = JSON.parse(res.value.toString())
+            if (organ.status === status){
+                results.push(organ)
+            }
+                
+        }
+        return JSON.stringify(results)
+    }
+
+    // helper function  which accepts a key and returns true if a key exits with a value 
+    async _keyExists(ctx, compositeKey){
+        const assetJSON = await ctx.stub.getState(compositeKey);
+        return assetJSON && assetJSON.length >0;
+    }
 
 }
 
